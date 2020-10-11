8bada7b2ac8e3e3fffc4b95faf69a9ec4335b70a
Ming Man Chan <chanmmn@hotmail.com>
2020-10-11 19:11:14 +0800

    Commit Math Metrix Multiply
    Commit Math Metrix Multiply

---
+++ ConAppMath/ConAppMath/Program.cs
@@ -0,0 +1,27 @@
+﻿using MathNet.Numerics.LinearAlgebra.Double;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ConAppMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            multiplicationExample();
+        }
+
+        private static void multiplicationExample()
+        {
+            DenseMatrix matrixA = DenseMatrix.OfArray(new[,] { { 2.0, 3.0 }, { 1.0, 4.0 } });
+            DenseMatrix matrixB = DenseMatrix.OfArray(new[,] { { 3.0, 2.0}, { 1.0, -6.0 } });
+
+            DenseMatrix result = matrixA * matrixB;
+            Console.WriteLine(result);
+        }
+    }
+}
